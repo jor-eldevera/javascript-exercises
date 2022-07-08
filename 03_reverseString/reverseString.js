fa238f9d1c790ae7e6fc8f94949713ed8fb831cc
jor-eldevera <jorell.devera@gmail.com>
2022-07-08 14:55:50 -0700

    Complete exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let ret = '';
 
+    for (let i = string.length - 1; i >= 0; i--) {
+        ret += string.charAt(i);
+    }
+
+    return ret;
 };
 
 // Do not edit below this line
