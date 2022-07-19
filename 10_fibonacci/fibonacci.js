026b3d975364d1f8bcdf7d67081da56bdb532986
jor-eldevera <jorell.devera@gmail.com>
2022-07-19 11:08:25 -0700

    Complete exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n === 1 || n === 2) {
+        return 1;
+    }
 
+    if (n < 0) {
+        return "OOPS";
+    }
+
+    n = n - 2;
+    let total = 1;
+    let previous = 1;
+    while (n > 0) {
+        let temp = total;
+        total = total + previous;
+        previous = temp;
+        n--;
+    }
+    return total;
 };
 
 // Do not edit below this line
