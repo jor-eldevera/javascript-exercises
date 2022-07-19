7c454d9f01dcac14649f781c7848084865a99f48
jor-eldevera <jorell.devera@gmail.com>
2022-07-18 22:33:24 -0700

    Complete exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let total = 0;
+	for (let item of array) {
+    total += item;
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let total = 1;
+	for (let item of array) {
+    total *= item;
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let ret = 1;
+  while (num > 0) {
+    ret *= num;
+    num--;
+  }
+  return ret;
 };
 
 // Do not edit below this line
